761bed29415c9e17be6c461e80ff94e762f18570
Umar Ali Off <umar.ali@evinciblesolutions.com>
2021-08-20 15:22:24 +0500

    Init

--- DynamicsGPEConnectXML/Form1.Designer.cs
+++ DynamicsGPEConnectXML/Form1.Designer.cs
@@ -28,63 +28,145 @@ namespace DynamicsGPEConnectXML
         /// </summary>
         private void InitializeComponent()
         {
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.debugInstructionsLabel = new System.Windows.Forms.Label();
+            this.CmbServerName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.helloWorldLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dbList = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.xmlEcon = new DLTextEditor.TextAreaControl();
+            this.excep = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // linkLabel1
+            // CmbServerName
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(191, 351);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(418, 20);
-            this.linkLabel1.TabIndex = 0;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Click here to continue learning how to build a desktop app!";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.CmbServerName.FormattingEnabled = true;
+            this.CmbServerName.Location = new System.Drawing.Point(15, 25);
+            this.CmbServerName.Name = "CmbServerName";
+            this.CmbServerName.Size = new System.Drawing.Size(237, 21);
+            this.CmbServerName.TabIndex = 0;
             // 
-            // debugInstructionsLabel
+            // label1
             // 
-            this.debugInstructionsLabel.AutoSize = true;
-            this.debugInstructionsLabel.Location = new System.Drawing.Point(146, 107);
-            this.debugInstructionsLabel.Name = "debugInstructionsLabel";
-            this.debugInstructionsLabel.Size = new System.Drawing.Size(532, 20);
-            this.debugInstructionsLabel.TabIndex = 1;
-            this.debugInstructionsLabel.Text = "Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app!";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(325, 240);
+            this.button1.Location = new System.Drawing.Point(258, 23);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 43);
+            this.button1.Size = new System.Drawing.Size(92, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Click Me!";
+            this.button1.Text = "Get DBs";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // helloWorldLabel
+            // label2
             // 
-            this.helloWorldLabel.AutoSize = true;
-            this.helloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.helloWorldLabel.Location = new System.Drawing.Point(303, 30);
-            this.helloWorldLabel.Name = "helloWorldLabel";
-            this.helloWorldLabel.Size = new System.Drawing.Size(192, 37);
-            this.helloWorldLabel.TabIndex = 3;
-            this.helloWorldLabel.Text = "Hello World!";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Database";
+            // 
+            // dbList
+            // 
+            this.dbList.Enabled = false;
+            this.dbList.FormattingEnabled = true;
+            this.dbList.Location = new System.Drawing.Point(393, 23);
+            this.dbList.Name = "dbList";
+            this.dbList.Size = new System.Drawing.Size(206, 21);
+            this.dbList.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 557);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Execute";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // xmlEcon
+            // 
+            this.xmlEcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xmlEcon.LineViewerStyle = DLTextEditor.Document.LineViewerStyle.FullRow;
+            this.xmlEcon.Location = new System.Drawing.Point(15, 74);
+            this.xmlEcon.Name = "xmlEcon";
+            this.xmlEcon.ShowEOLMarkers = true;
+            this.xmlEcon.ShowRightMargin = true;
+            this.xmlEcon.ShowSpaces = true;
+            this.xmlEcon.ShowTabs = true;
+            this.xmlEcon.Size = new System.Drawing.Size(584, 477);
+            this.xmlEcon.TabIndex = 7;
+            this.xmlEcon.UseAntiAliasFont = true;
+            // 
+            // excep
+            // 
+            this.excep.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.excep.Location = new System.Drawing.Point(15, 586);
+            this.excep.Name = "excep";
+            this.excep.ReadOnly = true;
+            this.excep.Size = new System.Drawing.Size(584, 115);
+            this.excep.TabIndex = 8;
+            this.excep.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 567);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Result/Exception";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "EConnect XML";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(404, 557);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Format XML";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.helloWorldLabel);
+            this.ClientSize = new System.Drawing.Size(611, 713);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.excep);
+            this.Controls.Add(this.xmlEcon);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dbList);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.debugInstructionsLabel);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmbServerName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Dynamics GP EConnect XML";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,10 +174,17 @@ namespace DynamicsGPEConnectXML
 
         #endregion
 
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Label debugInstructionsLabel;
+        private System.Windows.Forms.ComboBox CmbServerName;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label helloWorldLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox dbList;
+        private System.Windows.Forms.Button button2;
+        private DLTextEditor.TextAreaControl xmlEcon;
+        private System.Windows.Forms.RichTextBox excep;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- DynamicsGPEConnectXML/Form1.cs
+++ DynamicsGPEConnectXML/Form1.cs
@@ -1,12 +1,13 @@
+using Microsoft.Dynamics.GP.eConnect;
+using Microsoft.Dynamics.GP.eConnect.Serialization;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
@@ -18,18 +19,132 @@ namespace DynamicsGPEConnectXML
         public Form1()
         {
             InitializeComponent();
+            GetServerList();
         }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void GetServerList()
         {
-            // Click on the link below to continue learning how to build a desktop app using WinForms!
-            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
-
+            try
+            {
+                string myServer = Environment.MachineName;
+                DataTable servers = SqlDataSourceEnumerator.Instance.GetDataSources();
+                for (int i = 0; i < servers.Rows.Count; i++)
+                {
+                    if (myServer == servers.Rows[i]["ServerName"].ToString()) ///// used to get the servers in the local machine////
+                    {
+                        if ((servers.Rows[i]["InstanceName"] as string) != null)
+                            CmbServerName.Items.Add(servers.Rows[i]["ServerName"] + "\\" + servers.Rows[i]["InstanceName"]);
+                        else
+                            CmbServerName.Items.Add(servers.Rows[i]["ServerName"].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void GetDatabaseList()
+        {
+            try
+            {
+                string connectionString = "Data Source=" + CmbServerName.SelectedItem.ToString() + "; Integrated Security=True;";
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", con))
+                    {
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                dbList.Items.Add(dr[0].ToString());
+                            }
+                        }
+                    }
+                }
+                dbList.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
+            GetDatabaseList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                eConnectMethods eConnect = new eConnectMethods();
+                eConnectType eConnectType = new eConnectType();
+                var MyConnectionString = "Data Source=" + CmbServerName.SelectedItem.ToString() + ";Initial Catalog=xxxx;Persist Security Info=False;Integrated Security=SSPI;Min Pool Size=0;Max Pool Size=32767;Pooling=true;";
+                SqlConnectionStringBuilder MyConnectionStringBuilder = new SqlConnectionStringBuilder(MyConnectionString) { InitialCatalog = dbList.SelectedItem.ToString() };
+                eConnect.CreateTransactionEntity(MyConnectionStringBuilder.ConnectionString, xmlEcon.Text);
+                eConnect.Dispose();
+            }
+            catch (eConnectException ex)
+            {
+                excep.Text = ex.ToString();
+            }
+            catch (Exception ex)
+            {
+                excep.Text = ex.ToString();
+            }
+        }
+        public static string PrintXML(string xml)
+        {
+            string result = "";
+
+            MemoryStream mStream = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.Unicode);
+            XmlDocument document = new XmlDocument();
+
+            try
+            {
+                // Load the XmlDocument with the XML.
+                document.LoadXml(xml);
+
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Have to rewind the MemoryStream in order to read
+                // its contents.
+                mStream.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(mStream);
+
+                // Extract the text from the StreamReader.
+                string formattedXml = sReader.ReadToEnd();
+
+                result = formattedXml;
+            }
+            catch (XmlException)
+            {
+                // Handle the exception
+            }
+
+            mStream.Close();
+            writer.Close();
+
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(xmlEcon.Text))
+            {
+                string formated=PrintXML(xmlEcon.Text);
+                xmlEcon.Text = formated;
+            }
         }
     }
 }
